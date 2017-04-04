92a4d79e8dc5f1c4dc206b792796e5535da11efa
Barry Gackle <bdgackle@gmail.com>
2017-04-04 11:40:32 -0700

    WIP

--- src/bodytemp.cpp
+++ src/bodytemp.cpp
@@ -2,6 +2,8 @@
 
 /**
  * Temperature state of a single body part
+ *
+ * Contains all persistant state relevant to the body temperature model for one body part.
  */
 struct BodyPartTemperatures {
     /** Current temperature of this body part */
@@ -15,29 +17,47 @@ struct BodyPartTemperatures {
 }
 
 class BodyTemperature {
+        /**
+         * Add heat to body parts because of huddling over fire
+         *
+         * If the character is awake and actively attempting to warm up near a fire, then
+         * additional bonuses are applied to each body part.
+         *
+         * The strength of this effect is proportional to the size of the fire, and is
+         * stronger for hands and arms that can be held over the fire easily than for more
+         * central body parts.
+         *
+         * The feet are a special case.  A character sitting on a chair or similar is able
+         * to hold his or her feet over the fire, and gets a bonus.  A character standing
+         * by the fire gets little additional foot warming.
+         *
+         * @param fire_intensity Size of fire being used to warm up
+         * @param is_sitting If true, character can lift feet over fire
+         */
+        int add_bonus_fire_warmth( int fire_intensity, bool is_sitting );
+
+        /**
+         * Apply all types of "bonus" heat sources to all body parts.
+         *
+         * Certain types of warmth are "optional" for a character and are only applied if the
+         * body part is below the optimum temperature for comfort.  As an example, you wouldn't
+         * continue to hold your hands over the fire if the fire was too hot, so we don't apply
+         * that warmth source beyond the level of overheating.
+         */
+        void apply_bonus_warmth();
+
+        /**
+         * Update the model for one tick of heat flow.
+         *
+         * Causes the actual temperatures to converge toward their current end targets by one
+         * iteration.
+         */
+        void update_temperatures();
 
     private:
+        /** Temperature state data for all body parts */
         std::array<BodyPartModel, num_pb> bodyparts;
 };
-
-/**
- * Add heat to body parts because of huddling over fire
- *
- * If the character is awake and actively attempting to warm up near a fire, then
- * additional bonuses are applied to each body part.
- *
- * The strength of this effect is proportional to the size of the fire, and is
- * stronger for hands and arms that can be held over the fire easily than for more
- * central body parts.
- *
- * The feet are a special case.  A character sitting on a chair or similar is able
- * to hold his or her feet over the fire, and gets a bonus.  A character standing
- * by the fire gets little additional foot warming.
- *
- * @param fire_intensity Size of fire being used to warm up
- * @param is_sitting If true, character can lift feet over fire
- */
-int add_bonus_fire_warmth( int fire_intensity, bool is_sitting );
 
 /********************************************************************************/
 
@@ -82,5 +102,3 @@ void BodyTemperature::update_temperatures()
         bodyparts[i].current = int( diff *  + bodyparts[i]
     }
 }
-
-
