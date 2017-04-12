eeec0c4ce72c0a7abd41fd673d0fe0817a9cf55f
Barry Gackle <bdgackle@gmail.com>
2017-04-12 00:12:36 -0700

    WIP

--- src/bodytemp.cpp
+++ src/bodytemp.cpp
@@ -18,6 +18,16 @@ struct BodyPartTemperatures {
 
 class BodyTemperature
 {
+    public:
+        /**
+         * Set all body temperature parameters to a 'normal' value.  Calling this
+         * essentially disables body temperature effects.  Currently this is used to
+         * handle a debug command, but it could also be used in any other situation where
+         * it is desired to ignore temperature effects.
+         */
+        void set_to_normal();
+
+    private:
         /**
          * Add heat to body parts because of huddling over fire
          *
@@ -40,17 +50,22 @@ class BodyTemperature
         /**
          * Equalize all body parts
          *
-         * Heat slowly flows from warmer to colder body parts to simulate blood flow
+         * Heat slowly flows from warmer to colder body parts to simulate blood flow.  For
+         * the specific formula used, @see equalize_single_part().  This function exists to
+         * define which parts heat flows between.
          */
         void equalize_all_parts();
 
         /**
          * Do one tick worth of temperature equalization between body parts.
          *
-    f    * Calculates heat flow in one direction from a source to a sink body part.  The sink
-         * gets warmer if the source is warmer, and vice versa.
+         * Calculates heat flow in one direction from a source to a sink body part.  If the
+         * source is warmer than the sink, then the sink gets warmer.  If the source is cooler
+         * than the sink, then the sink gets cooler.  The temperature of the source is never
+         * changed, so bi-directional heat flow requires the function to be called twice, once
+         * for each direction.
          *
-         * Rate of flow is controlled by @ref BodyTemperature::equalization_factor
+         * Rate of flow is controlled by @ref BodyTemperature::equalization_factor.
          */
         void equalize_single_part( body_part sink, body_part source );
 
@@ -65,16 +80,19 @@ class BodyTemperature
         void apply_bonus_warmth();
 
         /**
-         * Update the model for one tick of heat flow.
+         * Update the model for one tick of heat flow from the environment
          *
          * Causes the actual temperatures to converge toward their current end targets by one
-         * iteration.
+         * iteration, simulating heat flow to/from the body and the surrounding environment.
+         * This function only handles heat flow due to ambient temperature.  It does not
+         * account for flow between body parts.  It also doesn't account for heat flows
+         * that the character automatically controls for comfort (such as huddling near a
+         * fire).
          */
         void update_temperatures();
 
-    private:
         /** Temperature state data for all body parts */
-        std::array<BodyPartModel, num_pb> bodyparts;
+        std::array<BodyPartModel, num_bp> bodyparts;
 
         /** How quickly body part temperatures equalize per tick */
         static const float equalization_factor;
@@ -83,6 +101,11 @@ class BodyTemperature
 /********************************************************************************/
 
 float BodyTemperature::equalization_factor = 0.0001;
+
+void BodyTemperature::set_to_normal()
+{
+    for( int i = 0; i < num_bp; i++ ) {
+}
 
 void BodyTemperature::add_bonus_fire_warmth( int fire_intensity, bool is_sitting )
 {
@@ -118,7 +141,7 @@ void BodyTemperature::apply_bonus_warmth()
 void BodyTemperature::update_temperatures()
 {
     // TODO
-    for( int i = 0; i < num_pb; i++ ) {
+    for( int i = 0; i < num_bp; i++ ) {
         // Do nothing if this part has already converged
         if( bodyparts[i].current == bodyparts[i].converging ) {
             continue;
@@ -130,9 +153,6 @@ void BodyTemperature::update_temperatures()
 
 void BodyTemperature::equalize_single_part( body_part sink, body_part source )
 {
-    // Body heat is moved around.
-    // Shift in one direction only, will be shifted in the other direction separately.
-    // If bp1 is warmer, it will lose heat
     int diff = bodyparts[source].current - bodyparts[sink].current;
     int flow = int( diff * equalization_factor );
     bodyparts[sink].current += flow;
@@ -140,48 +160,31 @@ void BodyTemperature::equalize_single_part( body_part sink, body_part source )
 
 void BodyTemperature::equalize_all_parts()
 {
-        // Flow from extremities to torso
-        equalize_single_part( bp_torso, bp_arm_l );
-        equalize_single_part( bp_torso, bp_arm_r );
-        equalize_single_part( bp_torso, bp_leg_l );
-        equalize_single_part( bp_torso, bp_leg_r );
-        equalize_single_part( bp_torso, bp_head );
+    // Flow from head and limbs to torso
+    equalize_single_part( bp_torso, bp_arm_l );
+    equalize_single_part( bp_torso, bp_arm_r );
+    equalize_single_part( bp_torso, bp_leg_l );
+    equalize_single_part( bp_torso, bp_leg_r );
+    equalize_single_part( bp_torso, bp_head );
 
-        case bp_head:
-            // 
-            equalize_single_part( bp_head, bp_torso );
-            equalize_single_part( bp_head, bp_mouth );
-            break;
-        case bp_arm_l:
-            equalize_single_part( bp_arm_l, bp_torso );
-            equalize_single_part( bp_arm_l, bp_hand_l );
-            break;
-        case bp_arm_r:
-            equalize_single_part( bp_arm_r, bp_torso );
-            equalize_single_part( bp_arm_r, bp_hand_r );
-            break;
-        case bp_leg_l:
-            equalize_single_part( bp_leg_l, bp_torso );
-            equalize_single_part( bp_leg_l, bp_foot_l );
-            break;
-        case bp_leg_r:
-            equalize_single_part( bp_leg_r, bp_torso );
-            equalize_single_part( bp_leg_r, bp_foot_r );
-            break;
-        case bp_mouth:
-            equalize_single_part( bp_mouth, bp_head );
-            break;
-        case bp_hand_l:
-            equalize_single_part( bp_hand_l, bp_arm_l );
-            break;
-        case bp_hand_r:
-            equalize_single_part( bp_hand_r, bp_arm_r );
-            break;
-        case bp_foot_l:
-            equalize_single_part( bp_foot_l, bp_leg_l );
-            break;
-        case bp_foot_r:
-            equalize_single_part( bp_foot_r, bp_leg_r );
-            break;
-        default:
+    // Flow from torso back to head and limbs
+    equalize_single_part( bp_head, bp_torso );
+    equalize_single_part( bp_arm_l, bp_torso );
+    equalize_single_part( bp_arm_r, bp_torso );
+    equalize_single_part( bp_leg_l, bp_torso );
+    equalize_single_part( bp_leg_r, bp_torso );
+
+    // Flow from extremeties to limbs and back
+    equalize_single_part( bp_arm_l, bp_hand_l );
+    equalize_single_part( bp_arm_r, bp_hand_r );
+    equalize_single_part( bp_leg_l, bp_foot_l );
+    equalize_single_part( bp_leg_r, bp_foot_r );
+    equalize_single_part( bp_hand_l, bp_arm_l );
+    equalize_single_part( bp_hand_r, bp_arm_r );
+    equalize_single_part( bp_foot_l, bp_leg_l );
+    equalize_single_part( bp_foot_r, bp_leg_r );
+
+    // Flow from mouth to head and back
+    equalize_single_part( bp_mouth, bp_head );
+    equalize_single_part( bp_head, bp_mouth );
 }
